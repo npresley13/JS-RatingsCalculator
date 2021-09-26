75ccc79926d005120e5cb4082ecc2338ddacd584
Nathan Presley <npresley13@gmail.com>
2021-09-26 12:58:27 -0400

    completed module

--- js/ratings.js
+++ js/ratings.js
@@ -1,29 +1,28 @@
 function collect_ratings() {
-    let rating = 0;
-    let ratings = {
-        count: 0,
-        sum: 0,
-        average: 0
+    const ratings = {
+        'count': 0,
+        'sum': 0,
+        'average': 0
     } 
+    let rating = 0;
     
     const elements = document.querySelectorAll(".rating");
-    elements.forEach(function (element) {
+    elements.forEach(element => {
         rating = parseInt(element.id.replace("star", ""));
         ratings.count += parseInt(element.value);
         ratings.sum += parseInt(element.value) * rating;
-    })  
+    });
     
     if (ratings.count !== 0) {
-        ratings.average = ratings.sum / ratings.count;
-        return ratings;
+        ratings.average = ratings.sum / ratings.count; 
     }
-}
+    return ratings;
+};
 
 
 document.addEventListener("change", () => {
-    collect_ratings();
     const ratings = collect_ratings();
-    ratings.average = document.querySelector("#average").value.toFixed(2);
+    ratings.average.toFixed(2) = document.querySelector("#average").value;
 });
 
 
